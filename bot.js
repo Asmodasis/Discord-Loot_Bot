1b98c929fba5c7aa6821ff73d0adabc414a34c31
Shawn Ray <asmodasis@gmail.com>
2020-06-12 16:38:42 -0700

    First commit

---
+++ bot.js
@@ -0,0 +1,57 @@
+const eris = require('eris');                                   // The class client for connecting a bot to discord.
+
+const botToken = 'NzIxMDk4NDU4NzU2NTQ2NTkx.XuPmpQ.EXM6QflwvrUpfLnTwOq6NMJuUuw';
+                                            
+const bot = new eris.Client(botToken);                          // Create a Client instance with our bot token.
+const commandPrefix = '~';                                      // The prefix to initialize the commands
+
+bot.on('ready', () => {                                         // When the bot is connected and ready, log to console.
+   console.log('Connected and ready.');
+});
+
+// Every time a message is sent anywhere the bot is present,
+// this event will fire and we will check if the bot was mentioned.
+// If it was, the bot will attempt to respond with "Present".
+bot.on('messageCreate', async (msg) => {
+   const botWasMentioned = msg.mentions.find(
+       mentionedUser => mentionedUser.id === bot.user.id,
+   );
+
+   if (botWasMentioned) {
+       try {
+           await msg.channel.createMessage('Present');
+       } catch (err) {
+           // There are various reasons why sending a message may fail.
+           // The API might time out or choke and return a 5xx status,
+           // or the bot may not have permission to send the
+           // message (403 status).
+           console.warn('Failed to respond to mention.');
+           console.warn(err);
+       }
+   }
+});
+
+bot.on('message', async (msg) => {
+    const botWasMentioned = msg.mentions.find(
+        mentionedUser => mentionedUser.id === bot.user.id,
+    );
+ 
+    if (botWasMentioned) {
+        try {
+            await msg.channel.createMessage('Present');
+        } catch (err) {
+            // There are various reasons why sending a message may fail.
+            // The API might time out or choke and return a 5xx status,
+            // or the bot may not have permission to send the
+            // message (403 status).
+            console.warn('Failed to respond to mention.');
+            console.warn(err);
+        }
+    }
+ });
+
+bot.on('error', err => {
+   console.warn(err);
+});
+
+bot.connect();
