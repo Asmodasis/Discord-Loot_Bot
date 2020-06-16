e5d6790caf152cd3429f826ff152dcb5faff3980
Shawn Ray <asmodasis@gmail.com>
2020-06-16 14:34:07 -0700

    Added help and loot messages, and handled mutliple messages

--- bot.js
+++ bot.js
@@ -1,66 +1,151 @@
 //Author: Shawn Ray
 
-const Discord = require('discord.js');                                                  // The class client for connecting a bot to discord.
+const Discord = require('discord.js');                                                          // The class client for connecting a bot to discord.                             
+var assert = require('assert')                                                                  // for handling assertions 
+const SynchronousPromise = require('synchronous-promise')
+
+const bot = new Discord.Client();                                                               // Create a Client instance with our bot token.
+const guild = new Discord.Guild(bot, bot.users);
+const commandPrefix = '~';                                                                      // The prefix to initialize the commands
+
+//var GoogleSpreadsheet = require('google-spreadsheet');                                          // for working with google spreadsheet
+var creds = require('./credentials.json')
+
 
 const botToken = '';
-                                            
-const bot = new Discord.Client();                                                       // Create a Client instance with our bot token.
-const commandPrefix = '~';                                                              // The prefix to initialize the commands
+const spreadID = '';
+const guildID = "";
+//var doc = new GoogleSpreadsheet(spreadID);  
 
-bot.on('ready', () => {                                                                 // When the bot is connected and ready, log to console.
+//const guild = new Discord.Guild(bot, bot.database.query)
+
+
+var hasRaidBegun = false;
+
+const priestRoleID = '<@&>';
+const warriorRoleID = '<@&>';
+const druidRoleID = '<@&>';
+const mageRoleID = '<@&>';
+const warlockRoleID = '<@&>';
+const hunterRoleID = '<@&>';
+const rogueRoleID = '<@&>';
+const paladinRoleID = '<@&>';
+
+bot.on('ready', () => {                                                                         // When the bot is connected and ready, log to console.
    console.log('Loot Assistant is connected and ready.');
+   //const guild = new Discord.Guild(bot, bot.database.query)
+  bot.user.setPresence({
+        game: { 
+            name: 'Loot Assistant',
+            type: 'WATCHING'
+        },
+        status: 'online'
+})
+   
 });
+
+var nick, us, ide;
 
 // Every time a message is sent anywhere the bot is present,
 // this event will fire and and check the valid commands and display
 // the appropriate responses
-bot.on('message',  (msg) => {
+bot.on('message', async (msg) => {
+    let time = new Date().toDateString();
+    if (msg.author.bot) return;                                                                 // if the author of the message is a bot, don't do anything
+    if(!msg.content.startsWith(commandPrefix)) return;                                          // only accept commands in the form of the prefix
 
-    if (msg.author.bot) return;                                                         // if the author of the message is a bot, don't do anything
-    if(!msg.content.startsWith(commandPrefix)) return;                                  // only accept commands in the form of the prefix
+    //console.log(msg.content.split(' ')[0]);
+    
+    //const g = guild.guilds.fetch(guildID);
+    //let guildMembers = msg.guild.members.fetch()
 
-   //console.log(msg.content.split(' ')[0]);
-
-   let args = msg.content.split(' ');
-    //let args = msg.content.slice(commandPrefix.length).trim().split(' ');              // Arguments, valid command example ~help will display a help message or ~say I am a bot, arg[0] would be I and arg[1] would be am
-     //let command = nil;                                           // force all arguments to lowercase for easy of use
-
+    let args = msg.content.split(' ');
+    //let args = msg.content.slice(commandPrefix.length).trim().split(' ');                     // Arguments, valid command example ~help will display a help message or ~say I am a bot, arg[0] would be I and arg[1] would be am
 
     for(var i = 0; i < args.length; ++i){
-        let command = args[i].toLowerCase();                                            // force all arguments to lowercase for easy of use
-        //console.log(command);                                                           // log the command to the console
-        switch(command) {                                                               // check the valid commands
+        let command = args[i].toLowerCase();                                                    // force all arguments to lowercase for easy of use
+        //console.log(i);
+        switch(command) {                                                                       // check the valid commands
         case "~help":
-                helpMessage(msg.author);                                                // display the help message to the invokers private message
+                helpMessage(msg.author);                                                        // display the help message to the invokers private message
+                //msg.channel.send(bot.channels.get(615984071402717214)) //.roles.get(priestRoleID));
             break;
-            case "~say":
-                msg.channel.createMessage('{Test} Say command indicated.')
-            break;
-            case "~loot":
-                //msg.channel.send(msg.author.username + ' has given '+ args[i+2] +' to ' + args[i+1]);
-                console.log(i)
-                i=applyLoot(msg, args[i+1], i+2, args)
+            case "~loot":                                                                       // handles loot distribution for the specified players
+                //console.log(i)
+                if(hasRaidBegun) applyLoot(msg, args[i+1], i+2, args, false);
+                else 
+                    msg.channel.send('A raid must be active before loot can be applied!');
             break;
             case "~retract":
-                msg.channel.createMessage('{Test} retract command indicated.')
+                if(hasRaidBegun) applyLoot(msg, args[i+1], i+2, args, true);
+                else 
+                    msg.channel.send('A raid must be active before loot can be applied!');
             break;
             case "~loot-history":
-                msg.channel.createMessage('{Test} loot-history command indicated.')
+                //console.log(getUserFromMention(msg.mentions.users.first().member.display_name))
+                //console.log(bot.guild.get(msg.mentions.users.first()));
+                //let mem = msg.guild.members.fetch()
+                //console.log(args[i+1])
+                //var nick, us, ide;
+               /*
+                const guildMembers = await new Promise((resolve, reject) => {                // fetches all the guild members
+                    if(!(msg.mentions.users.first()==null)) return resolve(msg.guild.members.fetch(msg.mentions.users.first()));
+                    else reject('Failure!');
+                    }).then((promise) => {
+                        nick        = (msg.mentions.users.first().username.users.nickname)
+                        us          = (msg.mentions.users.first().username)
+                        ide         = (msg.mentions.users.first().id)
+                        return msg.guild.members.fetch(msg.mentions.users.first())
+                    }).catch((err) => {console.log("Exception caught: " + err);});                                  
+                   
+                   // console.log(msg.guild.members.fetch(msg.mentions.users.first()))
+*/
+                let firstUser = msg.mentions.users.first()
+                let member = guild.member(firstUser);
+                let nick = member ? member.displayName : null;
+
+                console.log(args[i+1])
+                msg.channel.send(args[i+1]);
+ /*               console.log(msg.guild.members.fetch(msg.mentions.users.first()).then( (promise) => {
+                                nick = (msg.mentions.users.first().nickname)
+                                us = (msg.mentions.users.first().username)
+                                ide = (msg.mentions.users.first().id)
+                                return msg.guild.members.fetch(msg.mentions.users.first())
+                            }
+
+                let nick    = (guildMembers.nickname);
+                let us      = (guildMembers.username);
+                let ide     = (guildMembers.id);
+*/
+                //TODO: THESE PRINT BEFORE THE PROMISE IS RESOLVED
+               // console.log(nick)
+                //console.log(us)
+               // console.log(ide)
+               // msg.channel.send(nick);
+               // msg.channel.send(us);
+               // msg.channel.send(ide);
+
+                msg.channel.send('{Test} loot-history command indicated.')
+                //msg.channel.send(getUserFromMention(msg.mentions.users.first().member.display_name))
+                //msg.channel.send(bot.guild.get(msg.mentions.users.first()));
+                //msg.channel.send(getUserFromMention(args[i+1]))
             break;
             case "~guild-roster":
-                msg.channel.createMessage('{Test} guild-roster command indicated.')
+                msg.channel.send('The guild roster command is not operational currently.');
             break;
             case "~raid-roster":
-                msg.channel.createMessage('{Test} raid-roster command indicated.')
+                msg.channel.send('The raid roster command is not operational currently.');
             break;
             case "~attendance":
-                msg.channel.createMessage('The attendance command is not operational currently.')
+                msg.channel.send('The attendance command is not operational currently.');
             break;
             case "~start-raid":
-                msg.channel.createMessage('The start Raid command is not operational currently.')
+                hasRaidBegun = true;
+                msg.channel.send(msg.author.username + ' has begun the raid for ' + time + '. Good luck!');
             break;
             case "~end-raid":
-                msg.channel.createMessage('The end Raid command is not operational currently.')
+                hasRaidBegun = false;
+                msg.channel.send(msg.author.username + ' has ended the raid for ' + time);
             break;
         default: 
         
@@ -68,17 +153,73 @@ bot.on('message',  (msg) => {
     }
 });
 
+async function asyncForEach(array, callback) { // modifies version of forEach, taken from https://codeburst.io/javascript-async-await-with-foreach-b6ba62bbf404
+    for (let index = 0; index < array.length; index += 1) {
+        await callback(array[index], index, array);
+    }
+}
+
+
+function getUserFromMention(msg) {
+	if (!msg) return;
+    var returnClass = '';
+
+    //return bot.users.cache.get(msg);
+    //return msg;
+    //return bot.users.fetch(msg.author);
+    //return msg.mentions.first();
+
+
+       
+        
+        if (msg.startsWith('<@') && msg.endsWith('>')) {
+            msg = msg.slice(2, -1);
+    
+            if (msg.startsWith('!')) {
+                msg = msg.slice(1);
+            }
+                //return msg.author;
+                return bot.users.cache.get(msg).displayName;
+            
+            //return msg.guild.roles.get(msg.id);
+            //return bot.users.cache.get(msg).username;
+        }
+        
+    } 
+    /*   
+    if (bot.users.cache.get(msg) == undefined){
+        if(msg == priestRoleID){
+            returnClass = 'Priest';
+        }else if(msg == warriorRoleID){
+            returnClass = 'Warrior';
+        }else if(msg == druidRoleID){
+            returnClass = 'Druid';
+        }else if(msg == mageRoleID){
+            returnClass = 'Mage';
+        }else if(msg == warlockRoleID){
+            returnClass = 'Warlock';
+        }else if(msg == hunterRoleID){
+            returnClass = 'Hunter';
+        }else if(msg == rogueRoleID){
+            returnClass = 'Rogue';
+        }else if(msg == paladinRoleID){
+            returnClass = 'Paladin';
+        }
+        return returnClass;
+    }   
+    else{
+
+
+}*/
+
 function helpMessage (usr){
-    //msg.member.id.createMessage("{Test} Help command indicated.");
-    //usr.createMessage("{Test} Help command indicated.");
     usr.send("Hello and thank you for using the Loot Assistant bot.");
     usr.send("You have indicated that you would like assitance.");
     usr.send("The following are a list of commands that may be invoked.");
     usr.send("All commands must start with the control character indicated by '~', without the quotations.");
-    usr.send("'~say' followed by a list of words will tell the bot to say those words.");
     usr.send("'~loot' followed by an @user and [item text] will indicate that loot had been given to said user.");
     usr.send("'~retract' followed by an @user and [item text] will retract the item from the user.");
-    usr.send("'~loot-history' followed by an @user will display the loot history for that player.");
+    usr.send("'~loot-history' followed by an @user or @role will display the loot history for that player or class.");
     usr.send("'~guild-roster' will display the current guild roster.");
     usr.send("'~raid-roster' will display the current raid roster.");
     usr.send("'~start-raid' will begin a raid specified by the current date. It must be ended before the day is over." );
@@ -87,22 +228,40 @@ function helpMessage (usr){
 }
 
 
-function applyLoot (msg, usr, pos, args){
+function applyLoot (msg, usr, pos, args, isRetracted=false){
+    
     var i;
-    var itemString = '';                                                                       // The string for the loot item the player receives 
-    console.log(pos)
-    for(i = pos; i < args.length; ++i){                                                   // Start at the position in the command list when applyLoot is called
-        //if(args[i].startsWith(commandPrefix)) break;
-        itemString = itemString + (args[i]) + ' ';
+    var itemString = '';                                                                        // The string for the loot item the player receives 
+   
+    for(i = pos; i < args.length; ++i){                                                         // Start at the position in the command list when applyLoot is called
+        if(args[i].startsWith(commandPrefix)) break;                                            // all commands shall start with '~' if they do, it's not an (item)
+        itemString = itemString + (args[i]) + ' ';                                              // append all string literals into an (item)
     }
-    //console.log(itemString.type)
-    console.log(itemString)
 
-    msg.channel.send(msg.author.username + ' has given '+ itemString +' to ' + usr);
+
+
+    if(isRetracted){                                                                            // is the item being retracted from the document
+        console.log('The Item being removed is ' + itemString)                                  // display the string (item) to the console
+
+        msg.channel.send(msg.author.username + ' has retracted ' + itemString +' from ' + usr); // display the message to discord
+
+    }else{                                                                                      // then the item is being applied to the document
+        console.log('The Item being added is ' + itemString)                                    // display the string (item) to the console
+        _accessUserFromDoc()
+        msg.channel.send(msg.author.username + ' has given '+ itemString + ' to ' + usr);       // display the message to discord
+    }
+    
+    console.log(i);
+    
     return i;
 }
 
-function lootHistory (msg, usr){
+function lootHistory (msg, usr, playerClass, accessDate, isClassFlag=false){
+    if(isClassFlag){                                                                            // class mode, has to access all members of the class
+
+    }else{                                                                                      // individual user mode
+
+    }
 
 }
 function guildRoster (msg){
@@ -112,10 +271,11 @@ function raidRoster (msg){
 
 }
 
-function _accessUserFromExcel (usr){
+function _accessUserFromDoc (usr, accessDate){
 
 }
-function _accessUserFromExcel (usr){
+
+function _accessClassFromDoc (playerClass, accessDate){
 
 }
 
@@ -123,4 +283,4 @@ bot.on('error', err => {
    console.warn(err);
 });
 
-bot.login(botToken);                                                                          // connects the bot to the discord server.
+bot.login(botToken);                                                                             // connects the bot to the discord server.
